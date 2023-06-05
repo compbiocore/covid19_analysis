f0a1d7eabd148d31665fb45d5493aa71fe1f0d5b
Eric Salomaki <eric.salomaki@gmail.com>
2023-06-05 17:27:01 -0400

    Update run.sh

--- 1_scripts/run.sh
+++ 1_scripts/run.sh
@@ -7,29 +7,29 @@ mkdir -p ${pth}/3_results/${day}
 cd ${pth}/3_results/${day}
 
 # Run pangolin
-#pangolin $1 -o ${pth}/3_results/${day}/pangolin --alignment --no-temp
+pangolin $1 -o ${pth}/3_results/${day}/pangolin --alignment --no-temp
 
 # Run nextclade
-#nextclade dataset get -n sars-cov-2 -o nextclade_dataset
-#nextclade run \
-#	--input-dataset nextclade_dataset \
-#	--output-json ${pth}/3_results/${day}/nextclade.json \
-#	--output-csv ${pth}/3_results/${day}/nextclade.csv \
-#	--output-tsv ${pth}/3_results/${day}/nextclade.tsv \
-#	--output-tree ${pth}/3_results/${day}/nextclade.auspice.json \
-#	--input-qc-config ${pth}/2_metadata/qcRulesConfig.json \
-#	$1
-#	>${pth}/3_results/${day}/nextclade.log
+nextclade dataset get -n sars-cov-2 -o nextclade_dataset
+nextclade run \
+	--input-dataset nextclade_dataset \
+	--output-json ${pth}/3_results/${day}/nextclade.json \
+	--output-csv ${pth}/3_results/${day}/nextclade.csv \
+	--output-tsv ${pth}/3_results/${day}/nextclade.tsv \
+	--output-tree ${pth}/3_results/${day}/nextclade.auspice.json \
+	--input-qc-config ${pth}/2_metadata/qcRulesConfig.json \
+	$1
+	>${pth}/3_results/${day}/nextclade.log
 
 # Run nextalign
-#nextalign run \
-#	--genemap=${pth}/2_metadata/genemap.gff \
-#	--genes=E,M,N,ORF10,ORF14,ORF1a,ORF1b,ORF3a,ORF6,ORF7a,ORF7b,ORF8,ORF9b,S \
-#	--output-all=${pth}/3_results/${day}/nextalign \
-#	--input-ref=${pth}/2_metadata/reference.fasta \
-#	$1
+nextalign run \
+	--genemap=${pth}/2_metadata/genemap.gff \
+	--genes=E,M,N,ORF10,ORF14,ORF1a,ORF1b,ORF3a,ORF6,ORF7a,ORF7b,ORF8,ORF9b,S \
+  --output-all=${pth}/3_results/${day}/nextalign \
+	--input-ref=${pth}/2_metadata/reference.fasta \
+	$1
 
-#python3 ${pth}/1_scripts/qc.py ${day} ${pth}
+python3 ${pth}/1_scripts/qc.py ${day} ${pth}
 Rscript ${pth}/1_scripts/figures.R ${day} ${pth}
 Rscript ${pth}/1_scripts/RISHL_only_plots.R ${day} ${pth}
 Rscript ${pth}/1_scripts/new_perc_mutations.R ${day} ${pth}
