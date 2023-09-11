926ae21aef70acc7dabf98a83849468214b9ff0c
EricSalomaki <eric.salomaki@gmail.com>
2023-09-11 09:57:04 -0400

    Updated run_slurm.sh with new path for filesystem update

--- 1_scripts/run_slurm.sh
+++ 1_scripts/run_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 #SBATCH -n 24
 #SBATCH -N 1
-#SBATCH -t 10-00:00:00
+#SBATCH -t 2-00:00:00
 #SBATCH -J covid_pipeline
 #SBATCH --mem=128G
 
 pth=$(cd ../ && pwd)
 
-singularity exec -B /gpfs/data/ris3/ ${pth}/1_scripts/covid_new_pango.sif bash run.sh $1
+singularity exec -B /oscar/data/ris3/ ${pth}/1_scripts/covid_new_pango.sif bash run.sh $1
